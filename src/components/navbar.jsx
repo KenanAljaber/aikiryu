34dfd113c2093971d11085c1d3eeca41283a577e
KenanAljaber <keno12333@hotmail.com>
2023-12-12 10:41:15 +0100

    - responsive navbar added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.scss';
+import aikido from "../assets/icons/karate.png";
 function Navbar () {
+    const [isMobile, setIsMobile] = useState(false);
+    const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
+
+    useEffect(() => {
+      const handleResize = () => {
+        const windowWidth = window.innerWidth;
+        const isMobileDevice = windowWidth <= 768;
+        setIsMobile(isMobileDevice);
+      };
+  
+      handleResize();
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
+
     return (
         <>
         <div className="fake-navbar"></div>
-        <nav>
+        <img  onClick={() => setIsMobileMenuOpened(!isMobileMenuOpened)} className={'burger ' + (isMobile && isMobileMenuOpened ? 'burger-open' : '')} src={aikido} alt="akido icon" />
+        <nav className={isMobile && isMobileMenuOpened ? "open-navbar-mobile" : "navbar"}>
             <ul>
                 <li><a href="/about">Qui nous sommes ?</a></li>
                 <li><a href="/">Accueil</a></li>
